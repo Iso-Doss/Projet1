eb2fd8f495add54711e679835a39d64c7b2754b1
Iso-Doss <dossou.israel48@gmail.com>
2016-09-14 18:18:39 +0200

    Ajout de la Base de donnée a mon projet (fichier: gestionetudiant.sql

---
+++ gestionetudiant.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 14 Septembre 2016 à 18:16
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `gestionetudiant`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etudiant`
+--
+
+CREATE TABLE IF NOT EXISTS `etudiant` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(25) NOT NULL,
+  `prenom` varchar(25) NOT NULL,
+  `datenaissance` date NOT NULL,
+  `idfiliere` int(11) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=49 ;
+
+--
+-- Contenu de la table `etudiant`
+--
+
+INSERT INTO `etudiant` (`id`, `nom`, `prenom`, `datenaissance`, `idfiliere`, `pass`) VALUES
+(37, 'AFFOGBOLLO', 'Giovanni', '1996-02-11', 1, '3841895ea0c90753246b88084bc49c17613d06eb'),
+(38, 'MITCHOZONOU', 'Yann', '1996-12-15', 1, '48b3f0aebeed826ddacbdb90d06c485ff1180eee'),
+(39, 'joe', 'rock', '2005-05-09', 1, '6111a596e76070e4b00329e1a4851a7f30844921'),
+(48, 'doss', 'iso', '1970-01-01', 1, '8b817f1f6c7191b25103dfcbd8da27798d2a1dd8');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `filiere`
+--
+
+CREATE TABLE IF NOT EXISTS `filiere` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(25) NOT NULL,
+  `libelle` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `filiere`
+--
+
+INSERT INTO `filiere` (`id`, `code`, `libelle`) VALUES
+(1, 'IRT', 'Informatique RÃ©seau TÃ©l'),
+(2, 'CE', 'Communication Entreprise');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
